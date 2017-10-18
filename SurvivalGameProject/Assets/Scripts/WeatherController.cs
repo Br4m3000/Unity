15397c87b0e85cb634cf662191aa209def8d24bf
bram.sierhuis <bram@bramsierhuis.nl>
2017-10-18 21:09:44 +0200

    Partially working rain, cant turn on and off yet.

---
+++ SurvivalGameProject/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class WeatherController : MonoBehaviour {
+
+    [Header("Rain Settings")]
+    public GameObject rainParent;
+    public bool allowRain;
+
+    [Space(10)]
+    [Header("Debugging")]
+    public bool isRaining;
+
+    public bool startRain; //Button to start rain
+    public bool stopRain;
+
+    void Update()
+    {
+        if (startRain)
+        {
+            StartRain();
+            startRain = false;
+        }
+
+        if (stopRain)
+        {
+            StopRain();
+            stopRain = false;
+        }
+    }
+
+    void Start () {
+        isRaining = false;
+	}
+
+    public void StartRain()
+    {
+        if(!isRaining && allowRain && !rainParent.activeSelf)
+        {
+            Debug.Log("Start rain");
+            rainParent.SetActive(true);
+            isRaining = true;
+        }
+    }
+
+    public void StopRain()
+    {
+        if (isRaining && rainParent.activeSelf)
+        {
+            Debug.Log("Stop rain");
+            rainParent.SetActive(false);
+            isRaining = false;
+        }
+    }   
+}
